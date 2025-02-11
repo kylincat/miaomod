dfb8a072dfff805ac8cee1e28774f0b5703e4f81
shimakami <tl1476@126.com>
2025-02-11 21:44:16 +0800

    更新blockregistry

--- src/main/java/com/meow/miao/registry/BlockRegistry.java
+++ src/main/java/com/meow/miao/registry/BlockRegistry.java
@@ -4,14 +4,11 @@ import com.meow.miao.Miao;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class BlockRegistry {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(Miao.MODID);
-    public static final DeferredHolder<Block,Block> Miao_Block;
-    static {
-        Miao_Block = BLOCKS.register("miao_block",
-                () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.STONE).requiresCorrectToolForDrops().strength(2F, 1.5F)));
-    }
+    public static final DeferredBlock<Block> Miao_Block = BLOCKS.register("miao_block",
+            () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.STONE).requiresCorrectToolForDrops().strength(2F, 1.5F)));
 }
