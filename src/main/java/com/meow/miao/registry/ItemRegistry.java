5fef89dc1b74bc9ea4dc2bbb4d0c011e4605f3bb
shimakami <tl1476@126.com>
2025-02-11 21:34:19 +0800

    紫黑方块版

--- src/main/java/com/meow/miao/registry/ItemRegistry.java
+++ src/main/java/com/meow/miao/registry/ItemRegistry.java
@@ -1,18 +1,13 @@
 package com.meow.miao.registry;
 
 import com.meow.miao.Miao;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
-import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ItemRegistry {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Miao.MODID);
-
-    public static final String SULFUR_DUST_ID = "miao_item";
-    public static final DeferredHolder<Item, Item> Miao_Item;
-
-    static {
-        Miao_Item = ITEMS.register(SULFUR_DUST_ID,
-                () -> new Item(new Item.Properties()));
-    }
+    public static final DeferredItem<Item> Miao_Item = ITEMS.register("miao_item", () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> Miao_Block_Item = ITEMS.register("miao_block_item", () -> new BlockItem(BlockRegistry.Miao_Block.get(), new Item.Properties()));
 }
